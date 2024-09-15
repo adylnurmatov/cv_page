3b248037dbfbd2234154cc9cf84d96a392b1a2c8
adyl <nurmatovadyl8@gmail.com>
2024-09-15 13:42:55 +0600

    refactor js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
-/*==================== SHOW MENU ====================*/
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
 
-    // Validate that variables exist
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
-            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('nav-toggle', 'nav-menu')
 
-/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
@@ -43,10 +38,8 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== SHOW SCROLL TOP ====================*/
 function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if (this.scrollY >= 200) scrollTop.classList.add('show-scroll');
     else scrollTop.classList.remove('show-scroll')
 }
